788c32abe518185dab32731da36262b64ae8828f
Rahul Kumar <52167870+wukong23-cloud@users.noreply.github.com>
2020-07-31 16:35:17 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//function expresion to select element
+
+const selectElement = (s) => document.querySelector(s);
+
+//open the menu  on click
+
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+//close the menu  on click
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
